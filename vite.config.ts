fe67382194cc79368a31a4ce9d8b7dfcc278f6d3
wisetechitcare <wisetechit.care@gmail.com>
2026-05-18 19:18:50 +0530

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -25,5 +25,21 @@ export default defineConfig({
   base: "/",
   build: {
     chunkSizeWarningLimit: 3000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom', 'react-router-dom', 'react-redux'],
+          'vendor-mui': ['@mui/material', '@mui/icons-material', '@mui/x-date-pickers'],
+          'vendor-charts': ['apexcharts', 'react-apexcharts', 'recharts', 'chart.js'],
+          'vendor-pdf': ['@react-pdf/renderer', 'jspdf', 'jspdf-autotable', 'pdf-lib', 'pdfmake'],
+          'vendor-forms': ['formik', 'react-hook-form', 'yup', 'zod'],
+          'vendor-table': ['material-react-table', 'react-table', '@tanstack/react-query'],
+          'vendor-maps': ['leaflet', 'react-leaflet', '@react-google-maps/api', '@vis.gl/react-google-maps'],
+          'vendor-calendar': ['@fullcalendar/react', '@fullcalendar/daygrid', '@fullcalendar/timegrid', '@fullcalendar/interaction', '@fullcalendar/multimonth'],
+          'vendor-ui': ['antd', '@mantine/core', '@mantine/dates', 'framer-motion', 'react-select'],
+          'vendor-utils': ['axios', 'dayjs', 'lodash', 'xlsx', 'papaparse', 'jszip'],
+        },
+      },
+    },
   },
 })
